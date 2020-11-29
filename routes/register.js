1169ad295a511b10cc67b93caa93865907730892
Paulo Santiago <p.santiago796@gmail.com>
2020-11-29 20:47:26 +0000

    create register page and required cookie-session and bcrypt dependencies

--- routes/register.js
+++ routes/register.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const router  = express.Router();
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(
+  cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+  })
+);
 
 module.exports = (db) => {
+
+
   // Gets register page
   router.get("/", (req, res) => {
-    res.send("Register Page OK!");
+    console.log("req.session: ", req.session);
+    const userId = req.session["user_id"];
+    console.log("userId: ", userId);
+    // const templateVars = {
+    //   user_id: users[req.session["user_id"]]
+    // };
+    res.render("register");//, templateVars);
   });
 
   // Post and Creates new user
   router.post("/", (req, res) => {
-    // Can't test yet, need to create register.ejs first
+    console.log(req.body);
     res.send("Posting to Register Page OK!");
 
     // Need to fix these to match the db
