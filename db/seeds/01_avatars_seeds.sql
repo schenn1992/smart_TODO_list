b88b2e4b374a869402dcfa44d08f4a0692aba62c
Natalia Martian <natalia.martian@gmail.com>
2020-11-29 01:47:52 +0000

    Add user+avatar seeds

---
+++ db/seeds/01_avatars_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO avatars (image_url)
+VALUES
+('https://i.imgur.com/nlhLi3I.png'), --female - id1
+('https://i.imgur.com/z5LNkkB.png'), --female - id2
+('https://i.imgur.com/v0JXau2.png'), --female - id3
+('https://i.imgur.com/lRUnDgU.png'), --female - id4
+('https://i.imgur.com/3GvwNBf.png'), --female - id5
+('https://i.imgur.com/73hZDYK.png'), --male - id6
+('https://i.imgur.com/73hZDYK.png'), --male - id7
+('https://i.imgur.com/5fUVPRP.png'), --male - id8
+('https://i.imgur.com/DVpDmdR.png'), --male - id9
+('https://i.imgur.com/2WZtOD6.png'); --male - id10
