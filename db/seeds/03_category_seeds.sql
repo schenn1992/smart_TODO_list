cdb69333b380d812d2282d6292b45d78de6c83f9
Natalia Martian <natalia.martian@gmail.com>
2020-11-29 04:18:24 +0000

    Setup a few test items for each category + adapt schema to available API info out there

---
+++ db/seeds/03_category_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO movies (id, title, rating, synopsis)
+VALUES
+  ('Inception ', 8.8, 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.'),
+  ('News of the World', 0, 'A Civil War veteran agrees to deliver a girl, taken by the Kiowa people years ago, to her aunt and uncle, against her will. They travel hundreds of miles and face grave dangers as they search for a place that either can call home.'),
+  ('The Queen''s Gambit', 8.8, 'Orphaned at the tender age of nine, prodigious introvert Beth Harmon discovers and masters the game of chess in 1960s USA. But child stardom comes at a price.');
+
+
+INSERT INTO restaurants (id, name, rating, country, street, city, province, post_code)
+VALUES
+  ('Le Robin Square', 4.5, 'Canada', '300 Rue Notre_dame E', 'Montreal', 'QC', 'H2Y 1C7'),
+  ('Schwartz''s', 4.5, 'Canada', '3895 Boulevard Saint-Laurent', 'Montreal', 'QC', 'H2W 1X9'),
+  ('Au Pied de Cochon', 4.5, 'Canada', '536 Avenue Duluth E', 'Montreal', 'QC', 'H2L 1A9'),
+  ('Restaurant Park', 4.5, 'Canada', '378 Avenue Victoria', 'Montreal', 'QC', 'H3Z 2N4');
+
+INSERT INTO books (id, title, author, rating, synopsis)
+VALUES
+
+INSERT INTO products (id, name, rating, price)
+VALUES
