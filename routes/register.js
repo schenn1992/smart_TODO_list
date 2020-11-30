d0afc3d91f14eaa61a631023bc10a8ab226f27fb
Paulo Santiago <p.santiago796@gmail.com>
2020-11-30 20:23:38 +0000

    register post half working

--- routes/register.js
+++ routes/register.js
@@ -18,34 +18,34 @@ module.exports = (db) => {
   // Get register page
   router.get("/", (req, res) => {
     // Set the user id with the id in the database
-    const userId = getUserId(req.session["user_id"]);
-    const templateVars = {
-      user_id: userId
-    };
-    // renders the page, templateVars when cookies work
-    res.render("register", templateVars);//, templateVars);
+    // const userId = getUserId(req.session["user_id"]);
+    // const templateVars = {
+    //   user_id: userId
+    // };
+
+    return res.render("register");//, templateVars);
   });
 
-  // Add user to database
-  const addUser = (user) => {
-    const queryString = `
-        INSERT INTO users (username, email, password, avatar_id)
-        VALUES ($1, $2, $3, $4)
-        RETURNING *
-      `;
-    const values = [user.username, user.email, user.password, user.avatar_id];
-    return db.query(queryString, values)
-      .then(res => res.rows[0].email)
-      .catch(e => res.send(e));
-  };
-
   // Checks if an email is already in the database
-  const getUserEmail = (email) => {
+  const getUserByEmail = (email) => {
     const queryString = `
       SELECT * FROM users
       WHERE email = $1
       `;
     const values = [email];
+    return db.query(queryString, values)
+      .then(res => res.rows[0])
+      .catch(e => res.send(e));
+  };
+
+  // Add user to database
+  const addUser = (user) => {
+    const queryString = `
+    INSERT INTO users (username, email, password, avatar_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+  `
+    const values = [user.username, user.email, user.password, user.avatar_id];
     return db.query(queryString, values)
       .then(res => res.rows[0])
       .catch(e => res.send(e));
@@ -58,23 +58,33 @@ module.exports = (db) => {
     user.password = bcrypt.hashSync(user.password, 12);
     // Generate a random avatar id for the user
     user.avatar_id = Math.floor(Math.random() * Math.floor(9) + 1);
+
     // Checks if the submitted email and password were empty and sends an error
-    // if the email is already in use, send an error.
     if (!user.email || !user.password) {
       return res.status(400).send("Invalid email or password");
     }
-    
-    // Checks if the email is already in the database before registering the new account
-    // else if (getUserEmail(user.email) {
-    //   res.status(400).send("Email already in use");
+     // Checks if the email is already in the database before registering the new account
+    // if (getUserByEmail(user.email)) {
+    //   return res.status(400).send("Email already in use");
     // }
 
+    // Checks if the email is already in the database before registering the new account
+    getUserByEmail(user.email)
+      .then(email => {
+        if (email) {
+          return res.status(400).send("Email already in use");
+        }
+      });
+
     // Adds the user to the database
-    addUser(user);
-    // Sets cookie to the user's id
-    req.session.userId = user.id;
-    // Redirect to user's todo list after registering
-    res.redirect("/");
+    addUser(user)
+      .then(user => {
+        // Sets cookie to the user's id
+        req.session.userId = user.id;
+        // Redirect to user's todo list after registering
+        return res.redirect("/");
+      })
+      .catch(e => res.send(e));
   });
 
   return router;
