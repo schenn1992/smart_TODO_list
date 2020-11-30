00212caee1d4c223c0d021925d5f700b63405b1b
Steven <schenn1992@gmail.com>
2020-11-30 03:22:11 +0000

    modify login route to check for correct user login

--- routes/login.js
+++ routes/login.js
@@ -1,22 +1,41 @@
 const express = require('express');
+const cookieSession = require("cookie-session");
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const { getUserByEmail } = require ("../lib/helpers.js");
+const app = express();
+
+
+let users = {};
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
 
 module.exports = (db) => {
   // Get login page
-  router.get("/login", (req, res) => {
-    res.render("login_page");
+  router.get("/", (req, res) => {
+    res.render("login");
   });
 
   // Posts the user's login information and checks to see if it matches with the database
-  router.post("/login", (req, res) => {
-    const {email, password} = req.body;
+  router.post("/", (request, response) => {
+    const userInputEmail = request.body.email;
+    const userInputPassword = request.body.password;
+    const user = getUserByEmail(userInputEmail, users);
+    console.log("email: ",  userInputEmail);
+    console.log("password: ", userInputPassword);
+    console.log("user: ", user);
+    if (user && bcrypt.compareSync(userInputPassword , user.password)) {
+      request.session.user_id = user.id;
+      res.redirect("/");
+    } else {
+      return response.send("403 error. Please enter valid email or password");
+    }
 
-    // login does not exist yet, need to create a login helper function
-    login(email, password)
-      .then(user => {
-        if (!user) return res.send({error: "error"});
-      })
-    });
+  });
 
   return router;
+
 }
