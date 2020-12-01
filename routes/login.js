2aad98913b07da0cc78b276705283a8d99c4c7f0
Steven <schenn1992@gmail.com>
2020-12-01 01:10:46 +0000

    update route login page

--- routes/login.js
+++ routes/login.js
@@ -19,21 +19,37 @@ module.exports = (db) => {
     res.render("login");
   });
 
-  // Posts the user's login information and checks to see if it matches with the database
-  router.post("/", (request, response) => {
-    const userInputEmail = request.body.email;
-    const userInputPassword = request.body.password;
-    const user = getUserByEmail(userInputEmail, users);
-    console.log("email: ",  userInputEmail);
-    console.log("password: ", userInputPassword);
-    console.log("user: ", user);
-    if (user && bcrypt.compareSync(userInputPassword , user.password)) {
-      request.session.user_id = user.id;
-      res.redirect("/");
-    } else {
-      return response.send("403 error. Please enter valid email or password");
-    }
+  const getUserByEmail = (email) => {
+    const queryString = `
+      SELECT * FROM users
+      WHERE email = $1
+    `;
+    const values = [email];
+    return db.query(queryString, values)
+      .then(res => {
+        console.log(res.rows[0]);
+        return res.rows[0];
+      })
+      .catch(e => res.send(e));
+  };
 
+  // Posts the user's login information and checks to see if it matches with the database
+  router.post("/", (req, res) => {
+    const { email, password } = req.body;
+    const user = getUserByEmail(email); // return object with user email/password
+    console.log("input email: ", email);
+    console.log("input password: ", password);
+    console.log("user.email: ", user.email);
+    if (user.email === email) {
+      console.log("email check passed");
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log("password check passed");
+        req.session.user_id = user.id;
+        return res.redirect("/");
+      }
+    } else {
+      return res.send("403 error. Please enter valid email or password");
+    }
   });
 
   return router;
