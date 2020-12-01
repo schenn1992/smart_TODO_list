10cd892249b99143cb4101ec515e9f3b2287672e
Natalia Martian <natalia.martian@gmail.com>
2020-12-01 04:43:08 +0000

    Take out AJAX functionality, implement users route in node.js (route fully functional, though ugly)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,37 +30,33 @@ $(document).ready(() => {
     const email = $('#email').val();
     const password = $('#password').val();
 
-    //to be moved to helper file once I figure out how to import/export
-    //checks that all fields have input, so there are no NULL values sent to db
-    const verifyNoUserInput = function(input1, input2, input3) {
-      if(!input1 || !input2 || !input3) {
-        return false;
-      }
-      return true;
-    }
+    // //to be moved to helper file once I figure out how to import/export
+    // //checks that all fields have input, so there are no NULL values sent to db
+    // const verifyUserInput = function(input1, input2, input3) {
+    //   if(!input1 || !input2 || !input3) {
+    //     return false;
+    //   }
+    //   return true;
+    // }
 
-    if(verifyNoUserInput(username, email, password)) {
+    // if(verifyUserInput(username, email, password)) {
 
-      console.log('AJAX POST call');
+    //   console.log('AJAX POST call');
 
-      $.ajax({
-        url:`/users/${userId}`,
-        method: 'POST',
-        data: $('form').serialize()
-      })
-      .then(res => {
-        //display updated values on form after POST
-        // $('#username').val('')
-        // $('#email').val('')
-        // $('#password').val('')
-
-        location.reload();
-      })
-      .catch(err => console.log(err))
-      .always(() => console.log('AJAX POST successful'))
-    } else {
-      return alert('Fields cannot be empty!');
-    }
+    //   $.ajax({
+    //     url:`/users/${userId}`,
+    //     method: 'POST',
+    //     data: $('form').serialize()
+    //   })
+    //   .then(res => {
+    //     //display updated values on form after POST
+    //     location.reload();
+    //   })
+    //   .catch(err => console.log(err))
+    //   .always(() => console.log('AJAX POST successful'))
+    // } else {
+    //   return alert('Fields cannot be empty!');
+    // }
 
   });
 
