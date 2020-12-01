43558b272333f0dfe1cac56d68d60740b7996019
Paulo Santiago <p.santiago796@gmail.com>
2020-12-01 20:14:48 +0000

    create helper functions to check which api search to use

--- routes/smartPost.js
+++ routes/smartPost.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
+const {
+  movieSearch,
+  restaurantSearch,
+  bookSearch,
+  productSearch,
+  searchMovie} = require('../lib/helpers');
+
 module.exports = (db) => {
   // Gets form to add new item
   router.get("/", (req, res) => {
@@ -9,10 +16,20 @@ module.exports = (db) => {
 
   // Post new item
   router.post("/", (req, res) => {
-    const item = req.body.text;
-    console.log("user item input: ", item);
+    const userInput = req.body.text;
+
+    if (movieSearch(userInput.split(" "))) {
+      console.log("searchMovie: ", searchMovie(userInput));
+    }
+    console.log("searchMovie: ", searchMovie(userInput));
+    searchMovie(userInput);
+
 
     res.send("Adding new item OK!");
+    // console.log("movie search: ", movieSearch(userInput.split(" ")));
+    // console.log("restaurant search: ", restaurantSearch(userInput.split(" ")));
+    // console.log("book search: ", bookSearch(userInput.split(" ")));
+    // console.log("product search: ", productSearch(userInput.split(" ")));
   });
 
   return router;
