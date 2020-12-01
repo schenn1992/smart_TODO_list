557cbeeed50a3ebeb3321be233140d70bb9a960e
Paulo Santiago <p.santiago796@gmail.com>
2020-12-01 14:12:30 +0000

    create template for categories

--- routes/category.js
+++ routes/category.js
@@ -2,9 +2,54 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  // gives a list of the user's category
+  // category must be movies, restaurants, books, or products for query to work
+  const selectUserItems = (userId, category) => {
+    const queryString = `
+      SELECT *
+      FROM users_${category}
+      WHERE user_id = $2
+    `;
+    const values = [userId];
+    return db.query(queryString, values)
+      .then(res => {
+        console.log(res.rows);
+        return res.rows;
+      })
+      .catch(e => res.send(e));
+  }
+
   // Gets all categories list
   router.get("/", (req, res) => {
-    res.send("All Categories Page OK!");
+    // user req.session once user can login
+    // const userId = req.session["user_id"];
+    // temp setting user to 1
+    const userId = 1;
+    const categories = ["movies", "restaurants", "books", "products"];
+    let userList = [];
+
+    selectUserItems(userId, categories[0])
+      .then(res => {
+        console.log(res);
+        // res.forEach(element => userList.push(element))
+        // console.log("userlist: ", userList);
+      })
+      .catch(e => res.send(e));
+    console.log("user list: ", userList);
+
+    // categories.forEach(category => {
+    //   selectUserItems(userId, category)
+    //   .then(res => userList.forEach(element => userList.push(element)))
+    //   .catch(e => res.send(e));
+    //   });
+    // console.log(userList);
+
+    const templateVars = {
+
+    };
+
+    res.render("index", templateVars);
+    // res.send("All Categories Page OK!");
   });
 
   // Gets specific item page
