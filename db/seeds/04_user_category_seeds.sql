0c366f8715ed5b354efcf25fa79d7b71969054be
Paulo Santiago <p.santiago796@gmail.com>
2020-12-01 01:50:08 +0000

    user_category seeds syntax fix

--- db/seeds/04_user_category_seeds.sql
+++ db/seeds/04_user_category_seeds.sql
@@ -8,14 +8,14 @@ VALUES
   (3, 3),
   (3, 2),
   (4, 1),
-  (4, 3)
+  (4, 3),
   (5, 3),
   (5, 2),
-  (6, 2)
-  (6, 1)
+  (6, 2),
+  (6, 1);
 
 -- users_restaurants seeds
-INSERT INTO users_restaurants (user_id, movie_id)
+INSERT INTO users_restaurants (user_id, restaurant_id)
 VALUES
   (1, 2),
   (1, 3),
@@ -24,14 +24,14 @@ VALUES
   (3, 1),
   (3, 3),
   (4, 4),
-  (4, 3)
+  (4, 3),
   (5, 1),
   (5, 2),
-  (6, 2)
-  (6, 4)
+  (6, 2),
+  (6, 4);
 
 -- users_books seeds
-INSERT INTO users_restaurants (user_id, book_id)
+INSERT INTO users_books (user_id, book_id)
 VALUES
   (1, 2),
   (1, 3),
@@ -40,14 +40,14 @@ VALUES
   (3, 1),
   (3, 3),
   (4, 2),
-  (4, 3)
+  (4, 3),
   (5, 3),
   (5, 1),
-  (6, 2)
-  (6, 1)
+  (6, 2),
+  (6, 1);
 
   -- users_products seeds
-INSERT INTO users_restaurants (user_id, product_id)
+INSERT INTO users_products (user_id, product_id)
 VALUES
   (1, 2),
   (1, 3),
@@ -56,8 +56,8 @@ VALUES
   (3, 1),
   (3, 3),
   (4, 2),
-  (4, 3)
+  (4, 3),
   (5, 3),
   (5, 1),
-  (6, 2)
-  (6, 1)
+  (6, 2),
+  (6, 1);
